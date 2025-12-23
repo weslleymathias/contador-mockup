11de46310c969e758cf74c4a486e1b5ce52fe3e1
Weslley Mathias <weslleymathias@gmail.com>
2025-12-23 17:36:52 -0300

    fix-listagem-videos: altera e organiza listagem de vídeos

--- src/components/design2/NetworkSettings.tsx
+++
@@ -1,6 +0,0 @@
-import { NetworkSettings as NetworkSettingsOriginal } from '../NetworkSettings';
-
-// Reusing the original NetworkSettings component for Design 2
-export function NetworkSettings() {
-  return <NetworkSettingsOriginal />;
-}
